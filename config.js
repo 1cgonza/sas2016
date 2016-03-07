25cc27e571e2f1739933cd814c02f9b4864979bf
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-03-07 17:55:40 +0800

    fix urls to avoid redirects and OG complains

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-var baseUrl = 'http://localhost:3000/';
+var baseUrl = 'http://localhost:3000';
 var ogImg = 'SAS2016-OG-1200x630.jpg';
+var thumb = 'SAS2016-OG-500x300.jpg';
 
 var environments = {
   dev: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000',
     isDev: true
   },
   prod: {
-    baseUrl: 'http://sas2016.adm.ntu.edu.sg/',
+    baseUrl: 'http://sas2016.adm.ntu.edu.sg',
     isDev: false
   }
 };
@@ -17,38 +18,45 @@ var defaults = {
   author: 'Juan Camilo González',
   email: 'info@juancgonzalez.com',
   siteDescription: 'SAS Conference hosted by the School of Art, Design and Media, Nanyang Technological University of Singapore.',
-  defaultImage: baseUrl + 'img/' + ogImg,
+  defaultImage: baseUrl + '/img/' + ogImg,
+  defaultThumb: baseUrl + '/img/' + thumb,
   logos: {
     sas: {
       name: 'Society for Animation Studies',
       url: 'http://animationstudies.org/',
-      img: 'img/logo-sas.png'
+      img: 'img/logo-sas.png',
+      width: 86,
+      height: 45
     },
     adm: {
       name: 'School of Art, Design & Media',
       url: 'http://www.adm.ntu.edu.sg/',
-      img: 'img/logo-adm.svg'
+      img: 'img/logo-adm.svg',
+      width: 42,
+      height: 45
     },
     ntu: {
       name: 'Nanyang Technological University',
       url: 'http://www.ntu.edu.sg/',
-      img: 'img/logo-ntu.png'
+      img: 'img/logo-ntu.png',
+      width: 126,
+      height: 46
     }
   }
 };
 
-function attachOptions (options) {
+function attachOptions(options) {
   for (var option in options) {
     defaults[option] = options[option];
   }
-  defaults.defaultImage = options.baseUrl + 'img/' + ogImg;
+  defaults.defaultImage = options.baseUrl + '/img/' + ogImg;
   return defaults;
 }
 
-var defineEnvironment = function (args) {
+var defineEnvironment = function(args) {
   var env = attachOptions(environments.dev);
 
-  args.forEach(function (val) {
+  args.forEach(function(val) {
     if (val === '--prod' || val === '-p') {
       env = attachOptions(environments.prod);
     }
