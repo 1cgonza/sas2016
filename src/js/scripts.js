902bb7d807e73d3962f46050175d12c13567f74a
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-06-16 08:35:15 +0800

    cleanup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 function showLoadingContent() {
   var elements = document.querySelectorAll('.loading');
-  console.log(elements);
+
   for (var i = 0; i < elements.length; i++) {
     var ele = elements[i];
     ele.style.opacity = 1;
